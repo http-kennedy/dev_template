e6f56b01a697583bb00cb3113c6aebc52388c801
Kennedy <kennedy@mylaptop.dev>
2024-06-18 19:45:38 -0400

    update README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Dev_template
+# dev_template
+
+A module to quickly generate a basic Python project template.
+
+## Features
+
+* Creates a basic python project template
+* Initalizes a virtual environment for the project
+* Ability to add user-defined packages during setup
+
+## Installation
+
+To install the package, use pip:
+
+```bash
+pip3 install dev-template
+```
+
+## Usage
+
+After installing, you can run the module using:
+
+```bash
+python3 -m module.dev_template
+```
--- setup.py
+++ setup.py
@@ -3,13 +3,24 @@ import pathlib
 from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
+readme_path = HERE / "README.md"
+with open(readme_path, encoding="utf-8") as f:
+    long_description = f.read()
+
+HERE = pathlib.Path(__file__).parent
 requirements_path = HERE / "requirements.txt"
 with open(requirements_path, encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
     name="dev_template",
-    version="0.1.0",
+    version="0.1.1",
+    description="A template for Python project setup",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Kennedy",
+    author_email="kennedy@mylaptop.dev",
+    url="https://github.com/http-kennedy/dev_template",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=requirements,
@@ -19,4 +30,11 @@ setup(
         ],
     },
     extras_require={"test": ["pytest"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    license="CC0-1.0",
 )
