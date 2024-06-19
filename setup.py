7013b439235cf1ef7d2447b2efeb04df4688fe9a
Kennedy <kennedy@mylaptop.dev>
2024-06-19 00:08:23 -0400

    increment version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirements_path, encoding="utf-8") as f:
 
 setup(
     name="dev_template",
-    version="0.1.6",
+    version="0.1.7",
     description="A module to generate a Python project template.",
     long_description=long_description,
     long_description_content_type="text/markdown",
