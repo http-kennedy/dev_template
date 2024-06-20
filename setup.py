53b353a1c0f7c65e7fb0a1120ec57c990e161a04
Kennedy <kennedy@mylaptop.dev>
2024-06-19 20:39:50 -0400

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirements_path, encoding="utf-8") as f:
 
 setup(
     name="dev_template",
-    version="1.0.1",
+    version="1.0.2",
     description="A module to generate a Python project template.",
     long_description=long_description,
     long_description_content_type="text/markdown",
